312a15ddc130b1937bb5603862de2eedbeeeedaa
Serhii-Kysil <serik.kisel000@gmail.com>
2023-10-25 11:20:07 +0300

    Task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredient) => {
+  const liElement = document.createElement("li");
+  liElement.textContent = ingredient;
+  liElement.classList.add("item");
+  fragment.append(liElement);
+});
+
+list.appendChild(fragment);
