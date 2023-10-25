fd42422a6e563ba73ed8bb58a4e8b836c6cbe4f0
Serhii-Kysil <serik.kisel000@gmail.com>
2023-10-25 18:01:05 +0300

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const emailInput = formEl.elements.email;
+  const passwordInput = formEl.elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Усі поля повинні бути заповнені");
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log(formData);
+    formEl.reset();
+  }
+});
