4bf2549c0723790e642dadc9f688f3bc6c2588f9
Serhii-Kysil <serik.kisel000@gmail.com>
2023-10-26 12:02:26 +0300

    Task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,12 @@
 const fontSizeControlEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
+function defaultFontSize() {
+  textEl.style.fontSize = fontSizeControlEl.value + "px";
+}
+
 fontSizeControlEl.addEventListener("input", () => {
   textEl.style.fontSize = fontSizeControlEl.value + "px";
 });
+
+defaultFontSize();
